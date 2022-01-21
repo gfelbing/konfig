78a2d68559f05cbae29674c0aaf186c669b03f5a
Georg Welzel <georg.welzel@freiheit.com>
2022-01-21 14:37:23 +0100

    add license and doc

--- projects/examples/src/main/kotlin/de/gfelbing/konfig/examples/onefile/Main.kt
+++ projects/examples/src/main/kotlin/de/gfelbing/konfig/examples/onefile/Main.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019 Georg Felbinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.gfelbing.konfig.examples.onefile
 
 import de.gfelbing.konfig.core.source.ChainedKonfiguration
@@ -5,6 +21,11 @@ import de.gfelbing.konfig.core.source.EnvironmentKonfiguration
 import de.gfelbing.konfig.core.source.PropertiesFileKonfiguration
 import de.gfelbing.konfig.core.source.SystemPropertiesKonfiguration
 
+/**
+ * Sample maino to show off [SampleConfig].
+ *
+ * @author gfelbing@github.com
+ */
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
